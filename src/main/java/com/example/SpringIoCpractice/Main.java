7d66ff0846ca6ccd04a70b34fbbb3cd1317c5dbb
xiexinfang <1036554353@qq.com>
2019-07-26 15:46:35 +0800

    push

--- src/main/java/com/example/SpringIoCpractice/Main.java
+++ src/main/java/com/example/SpringIoCpractice/Main.java
@@ -1,1 +1,19 @@
-//TODO
+package com.example.SpringIoCpractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.context.support.FileSystemXmlApplicationContext;
+
+@SpringBootApplication
+public class Main {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HelloWorld.class, args);
+
+    ApplicationContext ctx = new FileSystemXmlApplicationContext("hello.xml");
+    // 2.从容器中获取Bean实例
+    HelloWorld ss = (HelloWorld) ctx.getBean("helloWorld");
+    ss.getHello();
+}}
